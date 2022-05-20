9fa81e6050568cbe78ff6aaffbf9f55e9abd1b5d
garcetek <garcetek@live.com>
2022-05-20 20:47:35 -0300

    Create gracias.js

---
+++ src/pages/gracias.js
@@ -0,0 +1,37 @@
+import React from "react"
+import Helmet from "react-helmet"
+import { graphql } from 'gatsby'
+import Layout from "../components/layout"
+
+const ContactPage = ({
+  data: {
+    site
+  },
+}) => {
+  return (
+    <Layout>
+      <Helmet>
+        <title>Gracias — {site.siteMetadata.title}</title>
+        <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
+      </Helmet>
+      <div className="two-grids -contact">
+        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
+          <h1 className="post-title">GRACIAS !!</h1>
+          <p>Ya recibimos tu mensaje y en breve nos ponemos en Contacto...</p>
+          <a href="/">Volver al Inicio</a>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+export default ContactPage
+export const pageQuery = graphql`
+  query ContactPageQuery{
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
