f39922bc6fbee949a88709a4e911d22f3a34851c
garcetek <garcetek@live.com>
2022-06-03 19:44:26 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `DIVA - Power-Pop`,
+        short_name: `DIVA`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
